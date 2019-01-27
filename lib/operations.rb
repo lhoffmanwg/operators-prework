3eb02f5ee411ef93307921745e2c5faf42e3d092
Laura Hoffman <lhoffmanwg@gmail.com>
2019-01-27 23:56:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+  if speed < 40 | speed > 60
+    return true  
+  else
+    return false
+  end
 end
 
 
 
-def not_safe?(speed)
-	
-end
+#def not_safe?(speed)
+#	 return speed  < 40 ? "true" : "false"
+#end
 	
 
 
